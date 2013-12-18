0c8817df86160b9182546b06a3014f92020d89e8
sander1 <sander@plexapp.com>
2013-12-18 23:46:38 +0100

    Use hosted resources, use function to create VideoClipObject.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,75 +1,56 @@
-import re
-
 TITLE = 'Jerry Seinfeld'
-
-ART = 'art-default.jpg'
-ICON = 'icon-default.png'
-
 BASE_URL = 'http://www.jerryseinfeld.com'
+RE_VIDEOS = Regex('data\("video", ([^\)]+?)\)')
 
 ###################################################################################################
-
 def Start():
-  Plugin.AddPrefixHandler('/video/jerryseinfeld', MainMenu, TITLE, ICON, ART)
-  Plugin.AddViewGroup('List', viewMode = 'List', mediaType = 'items')
 
-  # Set the default ObjectContainer attributes
   ObjectContainer.title1 = TITLE
-  ObjectContainer.view_group = 'List'
-  ObjectContainer.art = R(ART)
-
-  # Default icons for DirectoryObject and VideoClipObject in case there isn't an image
-  DirectoryObject.thumb = R(ICON)
-  DirectoryObject.art = R(ART)
-  VideoClipObject.thumb = R(ICON)
-  VideoClipObject.art = R(ART)
-
-  # Set the default cache time
   HTTP.CacheTime = CACHE_1HOUR
 
 ###################################################################################################
-
+@handler('/video/jerryseinfeld', TITLE)
 def MainMenu():
-  oc = ObjectContainer()
 
+  oc = ObjectContainer()
   page_content = HTTP.Request(BASE_URL).content
-  for video in re.finditer('data\("video", ([^\)]+?)\)', page_content):
-    video_details = JSON.ObjectFromString(video.groups()[0])
-    oc.add(VideoClipObject(
-      key = Callback(Lookup, video_url = video_details['mp4'], title = video_details['title'], thumb = video_details['jpg']),
-      rating_key = video_details['mp4'],
+
+  for video in RE_VIDEOS.finditer(page_content):
+    video_details = JSON.ObjectFromString(video.group(1))
+
+    oc.add(CreateVideoClipObject(
+      url = video_details['mp4'],
       title = video_details['title'],
-      thumb = video_details['jpg'],
-      items = [
-        MediaObject(
-          container = Container.MP4,
-          video_codec = VideoCodec.H264,
-          audio_codec = AudioCodec.AAC,
-          audio_channels = 2,
-          parts = [PartObject(key = video_details['mp4'])]
-        )
-      ]
+      thumb = video_details['jpg']
     ))
 
   return oc
 
 ###################################################################################################
+@route('/video/jerryseinfeld/createvideoclipobject')
+def CreateVideoClipObject(url, title, thumb, include_container=False):
 
-def Lookup(video_url, title, thumb):
-  oc = ObjectContainer()
-  oc.add(VideoClipObject(
-    key = Callback(Lookup, video_url = video_url),
-    rating_key = video_url,
+  videoclip_obj = VideoClipObject(
+    key = Callback(CreateVideoClipObject, url=url, title=title, thumb=thumb, include_container=True),
+    rating_key = url,
     title = title,
     thumb = thumb,
     items = [
       MediaObject(
+        parts = [
+          PartObject(key=url)
+        ],
         container = Container.MP4,
         video_codec = VideoCodec.H264,
+        video_resolution = 'sd',
         audio_codec = AudioCodec.AAC,
         audio_channels = 2,
-        parts = [PartObject(key = video_url)]
+        optimized_for_streaming = True
       )
     ]
-  ))
-  return oc
+  )
+
+  if include_container:
+    return ObjectContainer(objects=[videoclip_obj])
+  else:
+    return videoclip_obj
